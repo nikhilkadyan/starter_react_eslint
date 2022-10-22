ba7b3fc39c17f6dc208b8255b2f134a0e11d42d3
Nikhil Kadyan <nikhilkadyan@icloud.com>
2022-10-22 15:13:40 +0530

    fixed prettier

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React from "react"
+import React from 'react'
 
 function App() {
-  return (     
+  return (
     <div className="App">
-
       <p>React: ES Lint Starter</p>
     </div>
   )
